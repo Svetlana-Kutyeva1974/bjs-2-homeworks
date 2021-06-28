10534ad678bfd935bd3604f34ce6c02b8b2516b3
Svetlana Kutyova <kutyovas@yandex.ru>
2021-06-28 21:49:48 +0300

    1 commit  три задачи по фунциям раздел 2.function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,29 +3,52 @@ function getArrayParams(arr) {
   let min,max,sum,avg;
 
   // Ваш код
-
-  return { min:min, max:max, avg:avg };
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+  avg = 0;
+  // Ваш код
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i]>max) {
+    max = arr[i];
+  }
+    else if (arr[i]<min) {
+      min = arr[i];
+    }
+sum += arr[i];
+}
+avg += sum / arr.length;
+  return { min:min, max:max, avg:avg.toFixed(2) };
 }
 
 // Задание 2
+
 function worker(arr) {
-  let sum;
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   // Ваш код
-
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
+  let max = func(arrOfArr[0]);
   // Ваш кода
   // for ...
-  
-  return max
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
+  return max;
 }
 
 // Задание 3
+
 function worker2(arr) {
   // Ваш код
+  let rezult = getArrayParams(arr);
+  let differenceMaxMin = Math.abs(Math.abs(rezult.max) - Math.abs (rezult.min));
+  return differenceMaxMin;
 }
