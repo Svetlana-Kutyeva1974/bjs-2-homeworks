fbdee9cbc82bab1f9555598b790b407b7182c4d2
Svetlana Kutyova <kutyovas@yandex.ru>
2021-06-30 01:53:58 +0300

    1 commit 3.array task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,30 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+ let result = arr1.every((item, index, arr1) => {
+    let lengthArr = arr1.length;
+     if (arr1.length > arr2.length || arr1.length < arr2.length) {
+       return false;
+       }
+       else
+       {
+    if (arr1[index] === arr2[index])
+    {
+    return true;
+    }
+    else { 
+      return false;
+       } 
+     }
+  }
+);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let changeArr = arr.filter((item) => item >0 && item % 3 ===0);
+  let resultArr = changeArr.map((item, index) => {
+    const newArr = item * 10;
+    return newArr;
+  }
+  );
   return resultArr; // array
 }
