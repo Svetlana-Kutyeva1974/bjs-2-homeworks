ca200377e92bfce61e6d37d4a503e2cc9415f208
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-06-30 12:33:36 +0300

    2 commit one string

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,30 +1,10 @@
 function compareArrays(arr1, arr2) {
- let result = arr1.every((item, index, arr1) => {
-    let lengthArr = arr1.length;
-     if (arr1.length > arr2.length || arr1.length < arr2.length) {
-       return false;
-       }
-       else
-       {
-    if (arr1[index] === arr2[index])
-    {
-    return true;
-    }
-    else { 
-      return false;
-       } 
-     }
-  }
-);
-  return result; // boolean
+ return arr1.length === arr2.length && arr1.every((item, index, arr1) => {
+      return arr1[index] === arr2[index];
+      });
 }
 
 function advancedFilter(arr) {
-  let changeArr = arr.filter((item) => item >0 && item % 3 ===0);
-  let resultArr = changeArr.map((item, index) => {
-    const newArr = item * 10;
-    return newArr;
-  }
+  return arr.filter((item) => item > 0 && item % 3 === 0).map((item, index) => { return item * 10; }
   );
-  return resultArr; // array
 }
