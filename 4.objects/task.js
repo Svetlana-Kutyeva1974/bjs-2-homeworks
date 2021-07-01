8a1df8a940debad0f4e071d2bb88363c252ff760
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-07-01 23:22:43 +0300

    1 commit 4.object tasks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+ this.name = name;
+ this.gender = gender;
+ this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+    this.marks = [mark]; 
+    } else
+     {
+       this.marks[this.marks.length] = mark;
+    }
+};
+
+Student.prototype.addMarks = function (... mark) {
+  if (this.marks === undefined) { 
+    this.marks = [mark[0]];
+    } 
+   this.marks = mark.map((item, index, arr) => arr[index] = mark[index] );
+};
+
+
+Student.prototype.getAverage = function () {
+  return this.marks !== undefined && this.marks.reduce((acc, item, idx, arr) => {
+  acc += item;
+  if (idx === arr.length - 1) {
+    return acc / arr.length;
+  }
+    else {
+      return acc;
+    }
+})
+};
+
+Student.prototype.exclude = function(reason) {
+  delete  this.marks;
+  delete this.subject;
+  this.excluded = reason;
+};
