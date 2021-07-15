f64d4c5be6a0d4e05ce05b7a890de6b4e9f1856a
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-07-15 18:53:21 +0300

    3 commit (3*все заработало!!!)

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -33,60 +33,39 @@ fix() {
 }
 }
 
-
-
 class Magazine extends PrintEditionItem {
-   get type() {
-    return this._type;
-  }
-
-  set type(value) {
-     this._type = "magazine";
+  constructor(...args) {
+    super(...args);
+    this.type = "magazine";
   }
   }
 
-  class Book extends PrintEditionItem {
+class Book extends PrintEditionItem {
  constructor(author, ...args) {
     super(...args);
     this.author = author;
-  }
-
-   get type() {
-    return this._type;
-  }
-
-  set type(value) {
-     this._type = "book";
+    this.type = "book";
   }
   }
 
   class NovelBook extends Book {
-
-  get type() {
-    return this._type;
-  }
-  set type(value) {
-    this._type = "novel";
+    constructor(...args) {
+    super(...args);
+    this.type = "novel";
   }
 }
 
 class FantasticBook extends Book {
-
-  get type() {
-    return this._type;
-  }
-  set type(value) {
-    this._type = "fantastic";
+  constructor(...args) {
+    super(...args);
+    this.type = "fantastic";
   }
 }
 
 class DetectiveBook extends Book {
-
-  get type() {
-    return this._type;
-  }
-  set type(value) {
-    this._type = "detective";
+  constructor(...args) {
+    super(...args);
+    this.type = "detective";
   }
 }
 
@@ -99,17 +78,14 @@ class Library {
   }
   addBook(book) {
  if (book.state > 30) {
-  this.books[this.books.length] = book;
+  this.books.push(book);
+
  }
 }
 
 findBookBy(types, value) {
-  function getCharacter(types) {
-  return character => character[types] === value;
-}
- let p = '';
  let tt = {};
- tt = this.books.find(getCharacter(types));
+ tt = this.books.find(book => book[types] === value);
 return tt || null;
 }
 
@@ -124,121 +100,33 @@ giveBookByName(bookname) {
 }
 }
 
+}//library
 
-}
-
-/*
+//3*
 class Student {
   constructor(name) {
     this.name = name;
-    
-    //this.gender = gender;
-    //this.age = age;
-    this.objectOfMarks = [{subjectName: '', marks: []}];
-    //{subjectName: '', marks: []}
+    this.objectOfMarks = {};
   }
-
 
   addGrade(mark, subjectName) {
    
-    if (this.objectOfMarks[this.objectOfMarks.length-1].subjectName === undefined) {
-      
-      this.objectOfMarks[this.objectOfMarks.length-1].subjectName = subjectName;
-    
+    if (mark>0 && mark <6) {
+      if (!this.objectOfMarks[subjectName]) {
+      this.objectOfMarks[subjectName] = [];
+      }
+        this.objectOfMarks[subjectName].push(mark);
     }
     else {
-      if (mark>0 && mark <6){
-        this.addGrades(mark, subjectName);
-      }
-      else {
         alert(" Oшибка ввода оценки");
       }
-
-    }
-  }
-
-  addGrades(mark, subjectName) {
-    if (this.objectOfMarks.marks === undefined) {
-      // добавить первую оценку 
-      this.objectOfMarks.marks = [mark];
-      
-    } else {
-      //this.objectOfMarks.marks[this.objectOfMark.marks.length] = mark;
-      this.objectOfMarks.marks.push(mark);
-    }
-
-  }
-  */
-
-class Student {
-  constructor(name) {
-    this.name = name;
     
-    //this.gender = gender;
-    //this.age = age;
-    this.objectOfMarks = [];
-    //this.objectOfMarks = [{subjectName: '', marks: []}];
-    //{subjectName: '', marks: []}
-  }
-
-
-  addGrade(mark, subjectName) {
-   
-    if (this.objectOfMarks.length === 0 && mark>0 && mark <6) {
-     
-      this.objectOfMarks.push({[subjectName]: [mark]});
-      //this.objectOfMarks[this.objectOfMarks.length][subjectName] = subjectName;
-     
-
-    }
-    else {
-     if (mark>0 && mark <6){
-        this.addGrades(mark, subjectName);
-      }
-      else {
-        alert(" Oшибка ввода оценки");
-      }
-     
-    }
-  }
-
-  addGrades(mark, subjectName) {
-   
-   
-
-      for (let key in this.objectOfMarks) {
-        
-          for (let prop in this.objectOfMarks[key]){
-
-            if (prop === subjectName) {
-           
-            this.objectOfMarks[key][prop].push(mark); return;
-           }
-          
-            
-        
-          }
-      }
-       this.objectOfMarks.push({[subjectName]: [mark]});
-   }
-
+  }//func
  
-  /*getAverage() {
-    return this.objectOfMarks != undefined && this.objectOfMarks.reduce((acc, item, idx, arr) => {
-      acc += item;
-      if (idx === arr.length - 1) {
-        return acc / arr.length;
-      }
-      else {
-        return acc;
-      }
-    })
-  }*/
   getAverage() {
     let acc = 0;
     let length = 0;
     for (let key in this.objectOfMarks) {
-        console.log("ищем    " + key);
           for (let prop in this.objectOfMarks[key]){
             acc += this.objectOfMarks[key][prop]; 
             length += 1;
@@ -248,7 +136,7 @@ class Student {
    }
 
   getAverageBySubject(subjectName) {
-    return this.objectOfMarks != undefined && this.objectOfMarks.filter((item) => item === subjectName).reduce((acc, item, idx, arr) => {
+    return this.objectOfMarks != undefined && this.objectOfMarks[subjectName].reduce((acc, item, idx, arr) => {
       acc += item;
       if (idx === arr.length - 1) {
         return acc / arr.length;
@@ -257,8 +145,6 @@ class Student {
         return acc;
       }
     })
-
   }
-
-  
+ 
 }
