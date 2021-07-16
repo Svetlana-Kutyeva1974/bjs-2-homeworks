a37e338add576412038535c3e4637e6d9c6015e8
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-07-16 22:12:26 +0300

    1 commit for 7task

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,56 @@
+
+//const addThree = (a, b, c) => a + b + c;
+
+//---------------------------
 function cachingDecoratorNew(func) {
-  // Ваш код
+ let cache = [];
+ function wrapper(...args) {
+  const hash = args.join(','); // получаем правильный хэш
+    let idx = cache.findIndex((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+    if (idx !== -1 ) { // если элемент не найден
+        //console.log("Из кэша: " + cache[idx].value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+        return "Из кэша: " + cache[idx].value;
+      } else {
+        let result = func(...args); // в кэше результата нет - придётся считать
+        cache.push({["hash"]: hash ,["value"]: result}) ; // добавляем элемент с правильной структурой
+        if (cache.length > 5) { 
+          cache.shift();
+          //если слишком много элементов в кэше надо удалить самый старый (первый) 
+        }
+        //console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+      }
+    }
+    return wrapper;
+  }
+
+//2
+
+function debounceDecoratorNew(func, ms) {
+ let timeout;
+ return function (...args) {
+   clearTimeout(timeout);
+   timeout = setTimeout(() => {
+     f.apply(this, args);
+     console.timeEnd("tim");
+     console.time("tim");
+   }, ms);
+ };
 }
 
+//3
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+ let timeout;
+ function wrapper (...args) {
+   clearTimeout(timeout);
+   timeout = setTimeout(() => {
+     wrapper.conts ++;
+     return func.apply(this, args);
+     console.timeEnd("tim");
+     console.time("tim");
+   }, ms);
+ }
+ wrapper.conts = 0;
+ return wrapper;
 }
