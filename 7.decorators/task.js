9b22847e17617375c18b14f9e5f314af4286ffe1
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-07-19 01:46:51 +0300

    2 commit 7task

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,5 +1,5 @@
 
-//const addThree = (a, b, c) => a + b + c;
+const addThree = (a, b, c) => a + b + c;
 
 //---------------------------
 function cachingDecoratorNew(func) {
@@ -7,50 +7,53 @@ function cachingDecoratorNew(func) {
  function wrapper(...args) {
   const hash = args.join(','); // получаем правильный хэш
     let idx = cache.findIndex((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
-    if (idx !== -1 ) { // если элемент не найден
-        //console.log("Из кэша: " + cache[idx].value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-        return "Из кэша: " + cache[idx].value;
+      if (idx !== -1 ) { // если элемент найден
+          //console.log("Из кэша: " + cache[idx].value); 
+          return "Из кэша: " + cache[idx].value;
       } else {
-        let result = func(...args); // в кэше результата нет - придётся считать
-        cache.push({["hash"]: hash ,["value"]: result}) ; // добавляем элемент с правильной структурой
-        if (cache.length > 5) { 
-          cache.shift();
-          //если слишком много элементов в кэше надо удалить самый старый (первый) 
+          let result = func(...args); // в кэше нет
+            cache.push({["hash"]: hash ,["value"]: result}) ; // добавляем элемент 
+              if (cache.length > 5) { 
+                cache.shift();
+                //удалить самый старый (первый) 
+              }
+            //console.log("Вычисляем: " + result);
+            return "Вычисляем: " + result;  
         }
-        //console.log("Вычисляем: " + result);
-        return "Вычисляем: " + result;  
       }
-    }
     return wrapper;
   }
 
 //2
 
-function debounceDecoratorNew(func, ms) {
- let timeout;
- return function (...args) {
-   clearTimeout(timeout);
-   timeout = setTimeout(() => {
-     f.apply(this, args);
-     console.timeEnd("tim");
-     console.time("tim");
-   }, ms);
- };
+function debounceDecoratorNew(f, ms) {
+ let isIgnore = false;
+   return function (...args) {
+    if (isIgnore) return;
+    f.apply(this, args);
+    isIgnore = true;
+    setTimeout(() => { 
+      isCooldown = false;
+      f.apply(this, args);
+     // console.timeEnd("tim");
+    }, ms);
+  };
 }
 
 //3
 
 function debounceDecorator2(func, ms) {
- let timeout;
- function wrapper (...args) {
-   clearTimeout(timeout);
-   timeout = setTimeout(() => {
-     wrapper.conts ++;
-     return func.apply(this, args);
-     console.timeEnd("tim");
-     console.time("tim");
-   }, ms);
- }
- wrapper.conts = 0;
- return wrapper;
+  let isIgnore = false;
+    function wrapper (...args) {
+      if (isIgnore) return;
+      func.apply(this, args);
+      isIgnore = true;
+      setTimeout(() => {
+        isCooldown = false;
+        wrapper.conts ++;
+        return func.apply(this, args);
+      }, ms);
+    }
+  wrapper.conts = 0;
+  return wrapper;
 }
